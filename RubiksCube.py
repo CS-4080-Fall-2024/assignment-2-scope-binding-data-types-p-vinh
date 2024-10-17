a54f19bb462cec7945de6f0ae6baccab2f0c4b82
Vinh Pham <85326677+p-vinh@users.noreply.github.com>
2024-10-16 23:29:49 -0700

    Bug with adj faces

--- RubiksCube.py
+++ RubiksCube.py
@@ -9,86 +9,170 @@ class RubiksCube:
         """
         
          *        +-------+
-         *        | 8   9 | 
-         *        | 11 10 |
+         *        | White | 
+         *        |   U   |
          * +------+-------+-------+------+
-         * | 1  2 | 12 13 | 19 16 | 6  7 |
-         * | 0  3 | 15 14 | 18 17 | 5  4 | 
+         * |Green | Red   | Blue  |Orange|
+         * |  L   |   F   |   R   |  B   | 
          * +------+-------+-------+------+
-         *        | 22 23 |
-         *        | 21 20 |
+         *        | Yellow|
+         *        |  D    |
          *        +-------+
          
         """
         self.size = size
-        self.cube = np.zeros((6, size, size), dtype=object)
-        self.corner = np.zeros((8, 3), dtype=object)
-        self.edge = np.zeros((12, 2), dtype=object)
-        self.center = np.zeros((6, 1), dtype=object)
-        self.face = np.zeros((6, 1), dtype=object)
-        # Initialize the cube for debug as well
-        # Left side = 1L, Right side = 1R, Middle = 1M
+        self.faces = { 
+            'U': [['W'] * size for _ in range(size)],  # Up (White) 
+            'D': [['Y'] * size for _ in range(size)],  # Down (Yellow) 
+            'L': [['G'] * size for _ in range(size)],  # Left (Green) 
+            'R': [['B'] * size for _ in range(size)],  # Right (Blue) 
+            'F': [['R'] * size for _ in range(size)],  # Front (Red) 
+            'B': [['O'] * size for _ in range(size)],  # Back (Orange) 
+        } 
         
-        # Front side = 2F, Back side = 2B, Middle = 2M
-        
-        # Up side = 3U, Down side = 3D, Middle = 3M
-        for face in range(6):
-            for row in range(size):
-                self.cube[face, row, 0] = f"{face + 1}L"
-                self.cube[face, row, 1] = f"{face + 1}M"
-                self.cube[face, row, 2] = f"{face + 1}R"
             
+    def print_cube(self):
+        for face, grid in self.faces.items():
+            print(f"{face} face:")
+            for row in grid:
+                print(" ".join(row))
+            print()  
+        print("------")  
+
             
-    def rotate_layer(self, layer, axis, direction):
-        if axis == "x":
-            self._rotate_x(layer, direction)
-        elif axis == "y":
-            self._rotate_y(layer, direction)
-        elif axis == "z":
-            self._rotate_z(layer, direction)
-        else:
-            print("Invalid axis")
+    def rotate_face(self, face):
+        return [list(row) for row in zip(*face[::-1])]
 
-    # Up/down rotation
-    def _rotate_x(self, layer, direction):
-        temp = copy.deepcopy(self.cube[:, :, layer]) # Copy the layer to be rotated, only need 4 sides
-        # Can be any degree of 90, 180, 270 or 360. So we can just use the / operator to get the correct rotation
-        num_of_rot = direction // 90
+    def rotate_front(self): 
+        """Rotate the front face and the adjacent faces accordingly.""" 
+        # Rotate the front face 
+        self.faces['F'] = self.rotate_face(self.faces['F']) 
         
-
-        for i in range(0, self.size):
-            self.cube[0, layer, i] = temp[4, self.size - 1 - i]
-            self.cube[4, self.size - 1 - i, :] = temp[5, layer]
-            self.cube[5, layer, i] = temp[2, self.size - 1 - i]
-            self.cube[2, self.size - 1 - i, :] = temp[0, layer]
-            # self.cube[0, i, layer] = temp[layer, i]
-            # self.cube[1, i, layer] = temp[layer + 1, i]
-            # self.cube[2, i, layer] = temp[layer + 2, i]
-            # self.cube[3, i, layer] = temp[0, i]
+        # Rotate the adjacent edges 
+        top_row = self.faces['U'][2][:]
+        left_col = [self.faces['L'][i][2] for i in range(self.size)]
+        bottom_row = self.faces['D'][0][:]
+        right_col = [self.faces['R'][i][0] for i in range(self.size)]
         
-        # When rotating down, the faces are rotated in the opposite direction
-        
-        print(self.cube)
-        # print(temp)
-        # self.cube[4, ::-1, :] = temp[5, layer, :]
-        # self.cube[5, layer, :] = temp[2, ::-1, :]
-        # self.cube[2, ::-1, :] = temp[0, layer, :]
-        
-        # print(self.cube)
-        pass
+        # Update the adjacent faces
+        for i in range(self.size):
+            self.faces['U'][2][i] = left_col[self.size - 1 - i]
+            self.faces['L'][i][2] = bottom_row[i]
+            self.faces['D'][0][i] = right_col[self.size - 1 - i]
+            self.faces['R'][i][0] = top_row[i]
     
-    # Left/right rotation
-    def _rotate_y(self, layer, direction):
-        pass
-    
-    # Front/back rotation
-    def _rotate_z(self, layer, direction):
+    def rotate_back(self):
+        """Rotate the back face and the adjacent faces accordingly.""" 
+        # Rotate the back face 
+        self.faces['B'] = self.rotate_face(self.faces['B']) 
         
-        pass
+        # Rotate the adjacent edges 
+        top_row = self.faces['U'][0][:]
+        left_col = [self.faces['L'][i][0] for i in range(self.size)]
+        bottom_row = self.faces['D'][2][:]
+        right_col = [self.faces['R'][i][2] for i in range(self.size)]
+        
+        # Update the adjacent faces
+        for i in range(self.size):
+            self.faces['U'][0][i] = right_col[i]
+            self.faces['L'][i][0] = top_row[self.size - 1 - i]
+            self.faces['D'][2][i] = left_col[i]
+            self.faces['R'][i][2] = bottom_row[self.size - 1 - i]
+    
+    def rotate_up(self):
+        """Rotate the up face and the adjacent faces accordingly.""" 
+        # Rotate the up face 
+        self.faces['U'] = self.rotate_face(self.faces['U']) 
+        
+        # Rotate the adjacent edges 
+        front_row = self.faces['F'][0][:]
+        left_row = self.faces['L'][0][:]
+        back_row = self.faces['B'][0][:]
+        right_row = self.faces['R'][0][:]
+        
+        # Update the adjacent faces
+        for i in range(self.size):
+            self.faces['F'][0][i] = right_row[i]
+            self.faces['L'][0][i] = front_row[i]
+            self.faces['B'][0][i] = left_row[self.size - 1 - i]
+            self.faces['R'][0][i] = back_row[self.size - 1 - i]
+    
+    def rotate_down(self):
+        """Rotate the down face and the adjacent faces accordingly.""" 
+        # Rotate the down face 
+        self.faces['D'] = self.rotate_face(self.faces['D']) 
+        
+        # Rotate the adjacent edges 
+        front_row = self.faces['F'][2][:]
+        left_row = self.faces['L'][2][:]
+        back_row = self.faces['B'][2][:]
+        right_row = self.faces['R'][2][:]
+        
+        # Update the adjacent faces
+        for i in range(self.size):
+            self.faces['F'][2][i] = left_row[i]
+            self.faces['L'][2][i] = back_row[self.size - 1 - i]
+            self.faces['B'][2][i] = right_row[self.size - 1 - i]
+            self.faces['R'][2][i] = front_row[i]
+    
+    def rotate_left(self):
+        """Rotate the left face and the adjacent faces accordingly.""" 
+        # Rotate the left face 
+        self.faces['L'] = self.rotate_face(self.faces['L']) 
+        
+        # Rotate the adjacent edges 
+        top_col = [self.faces['U'][i][0] for i in range(self.size)]
+        front_col = [self.faces['F'][i][0] for i in range(self.size)]
+        bottom_col = [self.faces['D'][i][0] for i in range(self.size)]
+        back_col = [self.faces['B'][i][2] for i in range(self.size)]
+        
+        # Update the adjacent faces
+        for i in range(self.size):
+            self.faces['U'][i][0] = back_col[self.size - 1 - i]
+            self.faces['F'][i][0] = top_col[i]
+            self.faces['D'][i][0] = front_col[i]
+            self.faces['B'][i][2] = bottom_col[self.size - 1 - i]
+    
+    def rotate_right(self):
+        """Rotate the right face and the adjacent faces accordingly."""
+        # Rotate the right face 
+        self.faces['R'] = self.rotate_face(self.faces['R']) 
+        
+        # Rotate the adjacent edges 
+        top_col = [self.faces['U'][i][2] for i in range(self.size)]
+        front_col = [self.faces['F'][i][2] for i in range(self.size)]
+        bottom_col = [self.faces['D'][i][2] for i in range(self.size)]
+        back_col = [self.faces['B'][i][0] for i in range(self.size)]
+        
+        # Update the adjacent faces
+        for i in range(self.size):
+            self.faces['U'][i][2] = front_col[self.size - 1 - i]
+            self.faces['F'][i][2] = bottom_col[i]
+            self.faces['D'][i][2] = back_col[self.size - 1 - i]
+            self.faces['B'][i][0] = top_col[i]
 
+    def scramble(self, num_moves, seed=None):
+        np.random.seed(seed)
+        
+        for _ in range(num_moves):
+            move = np.random.choice(['F', 'B', 'U', 'D', 'L', 'R'])
+            if move == 'F':
+                self.rotate_front()
+            elif move == 'B':
+                self.rotate_back()
+            elif move == 'U':
+                self.rotate_up()
+            elif move == 'D':
+                self.rotate_down()
+            elif move == 'L':
+                self.rotate_left()
+            elif move == 'R':
+                self.rotate_right()
+                
 def main():
     cube = RubiksCube(3)
-    cube.rotate_layer(1, "x", 90) # Rotate the second layer in the x direction by 90 degrees
-
+    cube.print_cube()
+    cube.scramble(10)
+    cube.print_cube()
 if __name__ == "__main__":
     main()
