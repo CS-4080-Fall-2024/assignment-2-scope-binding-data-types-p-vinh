dee8f5dbf007b79d8466895b5a36a3ed5ad481b6
random-debug <85326677+p-vinh@users.noreply.github.com>
2024-10-18 22:46:02 -0700

    Debug Tools to help debug

--- RubiksCube.py
+++ RubiksCube.py
@@ -60,7 +60,7 @@ class RubiksCube:
             self.faces['L'][i][2] = bottom_row[i]
             self.faces['D'][0][i] = right_col[self.size - 1 - i]
             self.faces['R'][i][0] = top_row[i]
-    
+
     def rotate_back(self):
         """Rotate the back face and the adjacent faces accordingly.""" 
         # Rotate the back face 
@@ -92,11 +92,11 @@ class RubiksCube:
         
         # Update the adjacent faces
         for i in range(self.size):
-            self.faces['F'][0][i] = right_row[i]
+            self.faces['F'][0][i] = right_row[self.size - 1 - i]
             self.faces['L'][0][i] = front_row[i]
             self.faces['B'][0][i] = left_row[self.size - 1 - i]
-            self.faces['R'][0][i] = back_row[self.size - 1 - i]
-    
+            self.faces['R'][0][i] = back_row[i]
+
     def rotate_down(self):
         """Rotate the down face and the adjacent faces accordingly.""" 
         # Rotate the down face 
@@ -110,11 +110,11 @@ class RubiksCube:
         
         # Update the adjacent faces
         for i in range(self.size):
-            self.faces['F'][2][i] = left_row[i]
-            self.faces['L'][2][i] = back_row[self.size - 1 - i]
+            self.faces['F'][2][i] = left_row[self.size - 1 - i]
+            self.faces['L'][2][i] = back_row[i]
             self.faces['B'][2][i] = right_row[self.size - 1 - i]
             self.faces['R'][2][i] = front_row[i]
-    
+
     def rotate_left(self):
         """Rotate the left face and the adjacent faces accordingly.""" 
         # Rotate the left face 
@@ -132,7 +132,7 @@ class RubiksCube:
             self.faces['F'][i][0] = top_col[i]
             self.faces['D'][i][0] = front_col[i]
             self.faces['B'][i][2] = bottom_col[self.size - 1 - i]
-    
+
     def rotate_right(self):
         """Rotate the right face and the adjacent faces accordingly."""
         # Rotate the right face 
@@ -146,10 +146,10 @@ class RubiksCube:
         
         # Update the adjacent faces
         for i in range(self.size):
-            self.faces['U'][i][2] = front_col[self.size - 1 - i]
+            self.faces['U'][i][2] = front_col[i]
             self.faces['F'][i][2] = bottom_col[i]
             self.faces['D'][i][2] = back_col[self.size - 1 - i]
-            self.faces['B'][i][0] = top_col[i]
+            self.faces['B'][i][0] = top_col[self.size - 1 - i]
 
     def scramble(self, num_moves, seed=None):
         np.random.seed(seed)
@@ -168,11 +168,135 @@ class RubiksCube:
                 self.rotate_left()
             elif move == 'R':
                 self.rotate_right()
-                
+    
+    def isValidCube(self):
+        """Check if the cube is valid. A valid cube should have exactly 9 squares of each color."""
+        color_counts = {
+            'W': 0,  # White
+            'Y': 0,  # Yellow
+            'G': 0,  # Green
+            'B': 0,  # Blue
+            'R': 0,  # Red
+            'O': 0   # Orange
+        }
+
+        errors = []
+        
+        # Iterate through each face and count the colors
+        for face in self.faces.values():
+            for row in face:
+                for color in row:
+                    if color in color_counts:
+                        color_counts[color] += 1
+        
+        # Check if each color appears exactly 9 times
+        for color, count in color_counts.items():
+            if count != 9:
+                errors.append(f"{color} appears {count} times.\n")
+        
+        # Check edges
+        edges = [
+            ('U', 2, 1, 'F', 0, 1),
+            ('U', 1, 2, 'R', 0, 1),
+            ('U', 0, 1, 'B', 0, 1),
+            ('U', 1, 0, 'L', 0, 1),
+            ('D', 0, 1, 'F', 2, 1),
+            ('D', 1, 2, 'R', 2, 1),
+            ('D', 2, 1, 'B', 2, 1),
+            ('D', 1, 0, 'L', 2, 1),
+            ('F', 1, 2, 'R', 1, 0),
+            ('F', 1, 0, 'L', 1, 2),
+            ('B', 1, 2, 'R', 1, 2),
+            ('B', 1, 0, 'L', 1, 0)
+        ]
+        
+        for edge in edges:
+            face1, row1, col1, face2, row2, col2 = edge
+            if not self.is_valid_edge((self.faces[face1][row1][col1], self.faces[face2][row2][col2])):
+                errors.append(f"Invalid edge between {face1} face at ({row1}, {col1}): Color: {self.faces[face1][row1][col1]} \
+                                and {face2} face at ({row2}, {col2}): Color: {self.faces[face2][row2][col2]}\n")
+
+
+                # Check corners
+        corners = [
+            ('U', 2, 2, 'F', 0, 2, 'R', 0, 0),
+            ('U', 2, 0, 'F', 0, 0, 'L', 0, 2),
+            ('U', 0, 2, 'B', 0, 0, 'R', 0, 2),
+            ('U', 0, 0, 'B', 0, 2, 'L', 0, 0),
+            ('D', 2, 2, 'F', 2, 2, 'R', 2, 0),
+            ('D', 2, 0, 'F', 2, 0, 'L', 2, 2),
+            ('D', 0, 2, 'B', 2, 0, 'R', 2, 2),
+            ('D', 0, 0, 'B', 2, 2, 'L', 2, 0)
+        ]
+        
+        for corner in corners:
+            face1, row1, col1, face2, row2, col2, face3, row3, col3 = corner
+            if not self.is_valid_corner((self.faces[face1][row1][col1], self.faces[face2][row2][col2], self.faces[face3][row3][col3])):
+                errors.append(f"Invalid corner between {face1} face at ({row1}, {col1}), {face2} face at ({row2}, {col2}), and {face3} face at ({row3}, {col3})")
+        
+        if errors:
+            return False, errors
+        return True, []
+
+    def is_valid_edge(self, edge):
+        """Check if an edge piece is valid."""
+        valid_edges = {
+            ('W', 'R'), ('W', 'B'), ('W', 'O'), ('W', 'G'),
+            ('Y', 'R'), ('Y', 'B'), ('Y', 'O'), ('Y', 'G'),
+            ('R', 'G'), ('R', 'B'),
+            ('O', 'G'), ('O', 'B')
+        }
+        return edge in valid_edges or edge[::-1] in valid_edges
+
+    def is_valid_corner(self, corner):
+        """Check if a corner piece is valid."""
+        valid_corners = {
+            ('W', 'R', 'B'), ('W', 'R', 'G'), ('W', 'O', 'B'), ('W', 'O', 'G'),
+            ('Y', 'R', 'B'), ('Y', 'R', 'G'), ('Y', 'O', 'B'), ('Y', 'O', 'G')
+        }
+        return tuple(corner) in valid_corners
+    
+    def generate_map(self):
+        """Generate a map of the current state of the Rubik's Cube."""
+        size = self.size
+        map_str = ""
+
+        # Top face (U)
+        map_str += "         +-------+\n"
+        map_str += "         | White |\n"
+        map_str += "         |   Up  |\n"
+        for row in self.faces['U']:
+            map_str += "         | " + " ".join(row) + " |\n"
+        map_str += "         +-------+\n"
+
+        # Middle faces (L, F, R, B)
+        for i in range(size):
+            map_str += " +------+-------+-------+------+\n"
+            map_str += " | " + " ".join(self.faces['L'][i]) + " | " + " ".join(self.faces['F'][i]) + " | " + " ".join(self.faces['R'][i]) + " | " + " ".join(self.faces['B'][i]) + " |\n"
+        map_str += " +------+-------+-------+------+\n"
+
+        # Bottom face (D)
+        map_str += "         | Yellow|\n"
+        map_str += "         |  Down |\n"
+        for row in self.faces['D']:
+            map_str += "         | " + " ".join(row) + " |\n"
+        map_str += "         +-------+\n"
+
+        return map_str
+
 def main():
     cube = RubiksCube(3)
-    cube.print_cube()
-    cube.scramble(10)
-    cube.print_cube()
+    cube.rotate_front()
+    print(cube.generate_map())
+
+    
+    isValid, errors = cube.isValidCube()
+
+    if isValid:
+        print("The cube is valid.")
+    else:
+        print("The cube is invalid.")
+        for error in errors:
+            print(error)
 if __name__ == "__main__":
     main()
